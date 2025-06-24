12215baa184adf9eb04477148d2b8d8a5efbc2cf
AlexandraSkroba <aleksa1012016@gmail.com>
2025-06-24 20:58:15 +0300

    feat: update header

--- layout/Header/index.tsx
+++ layout/Header/index.tsx
@@ -1,11 +1,70 @@
-import { Text, View } from "react-native"
+import StyledText from "@/components/StyledText";
+import { COLORS } from "@/constants/ui";
+import { StyleSheet, View } from "react-native";
 
-const Header = () => {
-    return (
-        <View>
-            <Text>Header</Text>
+type HeaderProps = {
+  totalTodos: number;
+  completedTodos: number;
+};
+
+const Header: React.FC<HeaderProps> = ({ totalTodos, completedTodos }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.headerMainContent}>
+        {/* Левая часть - число */}
+        <StyledText style={styles.dataContent}>23</StyledText>
+
+        {/* Правая часть - день и месяц */}
+        <View style={styles.rightColumn}>
+          <StyledText style={styles.dayContent}>Monday</StyledText>
+          <StyledText style={styles.monthContent}>June</StyledText>
         </View>
-    )
-}
+      </View>
 
-export default Header
+      <StyledText style={styles.completedTodos}>
+        Completed: {completedTodos} / {totalTodos}
+      </StyledText>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 60,
+    paddingBottom: 15,
+    paddingHorizontal: 20,
+    backgroundColor: COLORS.SECONDARY_BACKGROUND,
+    alignItems: "center",
+    borderRadius: 20,
+  },
+  headerMainContent: {
+    marginBottom: 10,
+    flexDirection: "row",
+    gap: 10,
+  },
+  rightColumn: {
+    justifyContent: "center",
+  },
+  dataContent: {
+    fontSize: 70,
+    fontWeight: "bold",
+    color: COLORS.PRIMARY_TEXT,
+  },
+  dayContent: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: COLORS.PRIMARY_TEXT,
+  },
+  monthContent: {
+    fontSize: 40,
+    fontWeight: "bold",
+    color: COLORS.PRIMARY_TEXT,
+    marginTop: -10,
+  },
+  completedTodos: {
+    fontSize: 17,
+    color: COLORS.PRIMARY_TEXT,
+  },
+});
+
+export default Header;
